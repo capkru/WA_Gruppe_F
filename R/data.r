60c3c0ef1810142182505b41ef52a33ed7737804
Anas Salaheldin <anas.salaheldin@tu-dortmund.de>
2026-01-16 14:57:05 +0100

    Deck, Side und Cabin extrahieren / Spalten entfernt

--- R/data.r
+++ R/data.r
@@ -15,10 +15,9 @@
 #
 #
 
-# Schritt 1.4 
+# fehlende Werte imputieren und neue Variablen erstellen
 # 1. Daten laden (Pfad anpassen, falls nötig)
 titanic <- read.csv("data/titanic.csv")
-
 # 2. Anrede extrahieren
 # Wir suchen nach dem Wort, das zwischen dem Komma und dem Punkt im Namen steht
 titanic$Anrede <- sub(".*, (.*?)\\..*", "\\1", titanic$Name)
@@ -34,9 +33,8 @@ titanic$Pclass <- factor(titanic$Pclass,
                          levels = c(3, 2, 1), 
                          ordered = TRUE)
 
-# Testausgabe für Pclass
-# print("Check Pclass:")
-# print(head(titanic$Pclass))
+# ----Test---- print("Check Pclass:")
+# ----Test---- print(head(titanic$Pclass))
 
 # Wir berechnen den Median des Alters für jede Anrede-Gruppe
 # und füllen damit die Lücken (NA) im Alter auf
@@ -44,9 +42,24 @@ for (t in unique(titanic$Anrede)) {
   median_age <- median(titanic$Age[titanic$Anrede == t], na.rm = TRUE)
   titanic$Age[is.na(titanic$Age) & titanic$Anrede == t] <- median_age
 }
-
 # Falls noch NAs übrig sind (bei sehr seltenen Anreden), nehmen wir den Gesamt-Median
 titanic$Age[is.na(titanic$Age)] <- median(titanic$Age, na.rm = TRUE)
 
-print("Check Age (keine NAs mehr):")
-print(sum(is.na(titanic$Age)))
+# ----Test---- print(sum(is.na(titanic$Age)))
+
+# Deck und Side aus Cabin extrahieren 
+titanic$Cabin[titanic$Cabin == ""] <- NA
+titanic_numbers <- as.numeric(gsub("[^0-9]", "", titanic$Cabin))
+titanic$Side <- ifelse(is.na(titanic_numbers), NA,
+           ifelse(titanic_numbers %% 2==0,"Backbord", "Steuerbord"))
+titanic$Deck <- substr(titanic$Cabin, 1, 1)
+
+# ----Test----  print(head(titanic[, c("Cabin", "Deck", "Side"),10]))
+
+# Spalten entfernen , die nicht mehr benötigt werden
+col_to_remove <- c("Name", "Ticket", "Cabin", "PassengerId")
+# titanic_final <- titanic[, !(names(titanic) %in% col_to_remove)]
+
+# ---test--- print(colnames(titanic_finale))
+
+# write.csv(titanic_final, "data/titanic_cleaned.csv", row.names = FALSE)
