674aeea2ce0a4a02277b6067ce5b0e46b07a0f93
Grigorii Iakovlev <77197845+afgott@users.noreply.github.com>
2026-01-24 21:07:47 +0100

    2(a)+helper

--- R/descriptive_stats.r
+++ R/descriptive_stats.r
@@ -1,6 +1,21 @@
 ## Aufgabe 2(a)
-
+source("helper_functions.r") # Hilfsfunktionen laden
 ## (i) - Gregor
+stats_metric <- function(x) {
+    if (is.numeric(x)) {
+        cat("Lageparameter:\n") # nolint
+        print(berechne_lage(x))
+
+        cat("\nStreuungsmaße:\n")
+        print(berechne_streuung(x))
+        
+        cat("\nVerteilungsmaße:\n")
+        print(berechne_verteilung(x))
+
+        cat("\nMin und Max:\n")
+        print(berechne_extrema(x))
+    }
+}
 
 
 ## (ii) - Daniel
--- R/helper_functions.r
+++ R/helper_functions.r
@@ -1,2 +1,53 @@
 ## Aufgabe 2(b)
 
+## Hilfsfunktionen zu (i)
+
+# Lageparamaße berechnen
+berechneLage <- function(x) {
+  x <- na.omit(x) # NAs entfernen
+  list(
+    Mittelwert = mean(x),
+    Median = median(x),
+    Modus = berechneModus(x),
+    Quartile = quantile(x, probs = c(0.25, 0.5, 0.75))
+  )
+}
+
+# Streuungsmaße berechnen
+berechneStreuung <- function(x) {
+  x <- na.omit(x)
+  list(
+    Standardabweichung = sd(x),
+    Varianz = var(x),
+    Spannweite = diff(range(x)),
+    IQR = IQR(x), # Interquartilsabstand
+    Variationskoeffizient = sd(x) / mean(x)
+  )
+}
+
+# Verteilungsmaße berechnen
+berechneVerteilung <- function(x) {
+  x <- na.omit(x)
+  n <- length(x)
+  m <- mean(x)
+  s <- sd(x)
+  list(
+    Schiefe = sum((x - m)^3) / (n * s^3),
+    Kurtosis = sum((x - m)^4) / (n * s^4) - 3
+  )
+}
+
+# Min und Max berechnen
+berechneExtrema <- function(x) {
+  x <- na.omit(x)
+  list(
+    Minimum = min(x),
+    Maximum = max(x)
+  )
+}
+
+# Modus berechnen
+berechneModus <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
