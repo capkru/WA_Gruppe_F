7e262a352443126fc27bf4e7d04dd5eeb27788f3
Daniel Martel <denzel_martel@web.de>
2026-02-02 20:41:46 +0100

    Dateityp der kategorial Variablen in analysis.r zu factor umwandeln
    Anscheinend geht der factor-Datentyp bei CSV-Datein verloren. Jetzt werden die Variablen explizit in dem Skript zu factors umgewandelt.

--- R/analysis.R
+++ R/analysis.R
@@ -5,6 +5,12 @@ source("R/helper_functions.R")
 # aufgeraeumten Datensatz laden
 titanic <- read.csv("data/titanic_cleaned.csv")
 
+# CSVs speichern keine Faktoren. Wir müssen R sagen, was kategorial ist.
+titanic$Survived <- as.factor(titanic$Survived)
+titanic$Pclass   <- as.factor(titanic$Pclass)
+titanic$Sex      <- as.factor(titanic$Sex)
+titanic$Embarked <- as.factor(titanic$Embarked)
+
 # (i) Metric variables
 stats_metric(titanic$Age)
 stats_metric(titanic$Fare)
