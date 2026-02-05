0478702b59be8770d42fad500a55a40dcc19cfcd
Daniel Martel <144264838+capkru@users.noreply.github.com>
2026-02-05 14:28:07 +0100

    Verteilung der Ticketpreis zu analysis.r hinzufügen
    Ich habe ein Histogramm mit übergelegter Dichtekurve hinzugefügt.

--- R/analysis.R
+++ R/analysis.R
@@ -39,6 +39,49 @@ boxplot(titanic$Fare,
         col = "orange",
         main = "Boxplot of Fare")
 
+## Verteilung der Ticketpreise
+# 1. 95%-Quantil berechnen 
+limit_95 <- quantile(titanic$Fare, 0.95, na.rm = TRUE)
+
+# 2. Daten filtern: Nur Preise unterhalb dieser Grenze nutzen
+fare_sub <- titanic$Fare[titanic$Fare <= limit_95]
+
+# 3. Grafik erstellen
+hist(fare_sub, 
+     freq = FALSE,                
+     breaks = 20,                 
+     main = "Verteilung der Ticketpreise (95%-Interval)",
+     xlab = "Preis (Fare) in £",
+     col = "lightblue", 
+     border = "white")
+
+# 4. Dichtekurve darüberlegen
+lines(density(fare_sub), 
+      col = "darkred", 
+      lwd = 2)                    
+
+# 5. Median berechnen  
+med_val <- median(titanic$Fare, na.rm = TRUE) 
+
+# 6. Vertikale Linie zeichnen 
+abline(v = med_val, 
+       col = "blue", 
+       lwd = 2, 
+       lty = 2) # lty=2 macht die Linie gestrichelt
+
+# 7. Textbeschriftung an die Linie kleben
+text(x = med_val, 
+     y = 0.04,  # y-Position muss man evtl. anpassen je nach Höhe der Dichte
+     labels = paste("Median:", round(med_val, 2),"£"), 
+     pos = 4,   # 4 = rechts vom Punkt
+     col = "blue", 
+     cex = 0.8) # Schriftgröße etwas kleiner
+
+
+# 8. Rug hinzufügen
+rug(fare_sub, col = rgb(0, 0, 0, 0.1)) 
+
+
 # (ii) Categorical variables
 stats_categorical(titanic$Sex)
 stats_categorical(titanic$Survived)
@@ -58,11 +101,5 @@ plot_categorical_variables(
   "Sex",
   "Pclass"
 )
-
-# Piechart für die Ticket-Klassen
-piechart_categorical(titanic, "Pclass")
-
-# Piechart für den Hafen
-piechart_categorical(titanic, "Embarked")
 
 
